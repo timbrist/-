1d2093cac8b9085b8bdc9af80ec5963c1a78caf8
timbrist <timbrist@outlook.com>
2019-10-19 17:07:26 +0800

    add write file

--- main.c
+++ main.c
@@ -16,10 +16,22 @@ void ReadFile(const char *filename)
 
     fclose(fp);
 }
+void WriteFile(const char *filename,char *str)
+{
+    FILE *fp;
+    if( (fp = fopen(filename,"w")) ==NULL)
+    {
+        printf("Open file failed\n");
+        return;
+    }
+    fputs(str,fp);
+    fclose(fp);
+}
 
 int main()
 {
     printf("Hello world\n");
+    WriteFile("test.txt", "hhhhhhhhhhelo world\n");
     ReadFile("test.txt");    
     return 0;
 }
